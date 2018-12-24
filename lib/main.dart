bc162c698787b48fd306ee3908b07628527fd6bb
Alexey <43087954+BelodedAleksey@users.noreply.github.com>
2018-12-24 21:38:23 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'home_page.dart';
+import 'package:http/http.dart' as http;
+
+void main() async {
+  List currencies = await getCurrencies();
+  print(currencies);
+  runApp(new MyApp(currencies));
+}
+
+class MyApp extends StatelessWidget {
+  final List _currencies;
+
+  MyApp(this._currencies);
+
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      theme: new ThemeData(
+          primarySwatch: Colors.pink,
+          primaryColor: defaultTargetPlatform == TargetPlatform.iOS
+              ? Colors.grey[100]
+              : null),
+      home: new HomePage(_currencies),
+    );
+  }
+}
+
+Future<List> getCurrencies() async {
+  String cryptoUrl = "https://api.coinmarketcap.com/v1/ticker/?limit=50";
+  http.Response response = await http.get(cryptoUrl);
+  return jsonDecode(response.body);
+}
